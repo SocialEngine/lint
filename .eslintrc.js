eef38e7a40ed078b91f2e7108b1063e9a8752877
Raymond Benc <me@raybenc.com>
2018-02-09 13:34:07 +0100

    first

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "extends": "standard",
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+    "globals": {
+        "$App": true,
+        "Breeze": true,
+        "$": true,
+        "moment": true,
+        "fetch": true
+    },
+    "rules": {
+        "indent": ["error", 4, {"SwitchCase": 1}],
+        "semi": ["error", "always"],
+        "max-len": [1, 120, 4],
+        "no-alert": ["error"],
+        "space-before-function-paren": ["error", "always"]
+    },
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module"
+    }
+};

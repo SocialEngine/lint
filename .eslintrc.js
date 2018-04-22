5ea729eeaab704d4210d6c9a4a61482767125f6f
Raymond Benc <me@raybenc.com>
2018-04-22 15:51:08 +0200

    Adding babel-eslint as the parser.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,31 @@
 
 module.exports = {
-    "env": {
-        "node": true
+    'env': {
+        'node': true
     },
-    "extends": [
-        "standard",
-        "plugin:react/recommended"
+    'extends': [
+        'standard',
+        'plugin:react/recommended'
     ],
-    "plugins": [
-        "standard",
-        "promise",
-        "react"
+    'plugins': [
+        'standard',
+        'promise',
+        'react'
     ],
-    "rules": {
-        "indent": ["error", 4, {"SwitchCase": 1}],
-        "semi": ["error", "always"],
-        "max-len": [1, 120, 4],
-        "no-alert": ["error"],
-        "space-before-function-paren": ["error", "always"],
-        "react/react-in-jsx-scope": "off"
+    'parser': 'babel-eslint',
+    'rules': {
+        'indent': ['error', 4, {'SwitchCase': 1}],
+        'semi': ['error', 'always'],
+        'max-len': [1, 120, 4],
+        'no-alert': ['error'],
+        'space-before-function-paren': ['error', 'always'],
+        'react/react-in-jsx-scope': 'off'
     },
-    "parserOptions": {
-        "ecmaVersion": 2017,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
+    'parserOptions': {
+        'ecmaVersion': 2017,
+        'sourceType': 'module',
+        'ecmaFeatures': {
+            'jsx': true
         }
     }
 };
